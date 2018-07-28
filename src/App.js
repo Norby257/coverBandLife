adb1dedc1471f1cc5f8277ac7a558c9fa3b795a1
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-07-28 09:01:24 -0500

    initial ui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Container from './components/Container';
+// import Container from './components/Container';
+import Nav from './components/Nav';
 import Flexbox from 'flexbox-react';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <Container />
+    <Nav/>
        
       </div>
     );
