ed0febddd3bf319813a4a2629e18d26ccde08154
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-07-28 19:11:19 -0500

    updating yarn and package files

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 // import Container from './components/Container';
 import Nav from './components/Nav';
+import Header from './components/Header';
 import Flexbox from 'flexbox-react';
 
 class App extends Component {
@@ -11,12 +12,6 @@ class App extends Component {
       <div className="App">
 
       <Header />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
-    
-
       </div>
     );
   }
