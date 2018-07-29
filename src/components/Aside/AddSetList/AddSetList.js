182fba3d548c6ccf2c47383b88bac6a6e50c9474
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-07-29 08:02:36 -0500

    add-setlist component

---
+++ src/components/Aside/AddSetList/AddSetList.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react"
+
+class AddSetList extends Component {
+  render() {
+    return (
+      <div className="playlist">
+        <a href="#">
+          <i className="ion-ios-plus-outline"></i>
+           + New set list
+        </a>
+      </div>
+    )
+  }
+}
+
+export default AddSetList;
