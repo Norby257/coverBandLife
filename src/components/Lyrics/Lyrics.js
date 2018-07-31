dd94864a35fe8b2cf3dcc67d1a959e1b200110d3
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-07-30 21:04:49 -0500

    Lyrics component

--- src/components/Lyrics/Lyrics.js
+++ src/components/Lyrics/Lyrics.js
@@ -4,8 +4,13 @@ import './Lyrics.scss';
 class Lyrics extends Component {
     render() {
         return (
-            <div class="content__right">
+            <div className="content__right">
                 LYRICS ARE HERE 
+
+                <div className="track"> Track Name </div>
+                <div> Artist </div>
+                <div> Key </div>
+                <div> Chords</div>
             </div>
         )
     }
