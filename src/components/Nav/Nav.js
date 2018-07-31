75d61e3096bad95b65255a4ba8efc252055bd78f
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-07-30 20:26:27 -0500

    11-dropdown-menu-nav fixed

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,34 +5,8 @@ import './Nav.scss';
 
 const Nav = props => (
     <nav className="navigation">
-        <div className="navigation__list">
-
-          <div className="navigation__list__header" 
-               role="button" 
-               data-toggle="collapse" 
-               href="#main" 
-               aria-expanded="true" 
-               aria-controls="main">
-            Main
-          </div>
-          
-          <div className="collapse in" id="main">
-          
-            <a href="#" className="navigation__list__item">
-              <i className="ion-ios-browsers"></i>
-              <span>Browse</span>
-            </a>
-
-            <a href="#" className="navigation__list__item">
-              <i className="fas fa-chart-line"></i>
-              <span>Activity</span>
-            </a>
-
-            <a href="#" className="navigation__list__item">
-              <i className="fas fa-broadcast-tower"></i>
-              <span>Radio</span>
-            </a>
-            
+        <div className="navigation__list">      
+          <div className="collapse in" id="main">      
           </div>
 
         </div>
